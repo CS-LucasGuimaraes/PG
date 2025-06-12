a6412c78b9570d540d2a2004c64f5bd47c285397
Thiago-Ramalho <trm4@cin.ufpe.br>
2025-06-12 16:45:21 -0300

    feat(plane.hpp): add header file for plane object class

---
+++ libs/Prism/include/Prism/plane.hpp
@@ -0,0 +1,25 @@
+#ifndef PRISM_PLANE_HPP_
+#define PRISM_PLANE_HPP_
+
+#include "prism_export.h"
+#include "Prism/objects.hpp"
+#include "Prism/point.hpp"
+#include "Prism/vector.hpp"
+
+namespace Prism {
+
+class PRISM_EXPORT Plane : public Object {
+  public:
+    Plane(Point3 point_on_plane, Vector3 normal, Material* material);
+
+    virtual bool hit(const Ray& ray, ld t_min, ld t_max, HitRecord& rec) const override;
+
+  private:
+    Point3 point_on_plane;
+    Vector3 normal;
+    Material* material;
+};
+
+} // namespace Prism
+
+#endif // PRISM_PLANE_HPP_
