6a93b054728e6914755433fd9583dd344c8514ad
tomclima <tomecostalima1228@gmail.com>
2025-06-19 02:43:35 -0300

    Feat(material): added mtl-like information to material class (emmision, refraction, specularity, etc)

--- libs/Prism/include/Prism/material.hpp
+++ libs/Prism/include/Prism/material.hpp
@@ -8,9 +8,19 @@ namespace Prism {
 
     class PRISM_EXPORT Material {
         public:
-            Material(Color color) : color(color) {};
+
+        Material(Color color = Color(),  Vector3 ka = Vector3(), Vector3 ks = Vector3(), Vector3 ke = Vector3(), double ns = 0, double ni = 0 , double d = 0) : 
+            color(color), ka(ka), ks(ks), ke(ke), ns(ns), ni(ni), d(d) {}
 
             Color color;
+            Vector3 ka;
+            Vector3 ks;
+            Vector3 ke;
+            double ns;
+            double ni;
+            double d;
+
+
     };
 
 }  // namespace Prism
