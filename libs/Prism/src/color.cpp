74427137c05fe1cd13a831911b74e126b275f257
Lucas Guimarães <lgf@cin.ufpe.br>
2025-07-08 02:04:07 -0300

    fix(color): Correct floating-point division in Color constructor for accuracy

--- libs/Prism/src/color.cpp
+++ libs/Prism/src/color.cpp
@@ -4,7 +4,7 @@ namespace Prism {
 
 Color::Color() : r(0), g(0), b(0) {}
 Color::Color(double red, double green, double blue) : r(red), g(green), b(blue) {}
-Color::Color(int red, int green, int blue): r(static_cast<double>(red) / 255.0f), g(static_cast<double>(green) / 255.0f), b(static_cast<double>(blue) / 255.0f) {}
+Color::Color(int red, int green, int blue): r(static_cast<double>(red) / 255.0), g(static_cast<double>(green) / 255.0), b(static_cast<double>(blue) / 255.0) {}
 Color::Color(const Color& other) : r(other.r), g(other.g), b(other.b) {}
 
 } // namespace Prism
